eea829cace15d42b8a160853f393b4794dfd5c7d
tjandy98 <3953059+tjandy98@users.noreply.github.com>
2024-01-10 16:53:27 +0800

    Initial commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"text/template"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
+		"./ui/html/pages/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/pages/home.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.errorLog.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.errorLog.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+
+	w.Write([]byte("Hello World"))
+}
+
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	// w.Write([]byte("Display a specific snippet..."))
+}
+
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		// Set the Allow header to inform the client which HTTP methods are allowed
+		w.Header().Set("Allow", http.MethodPost)
+
+		// WriteHeader must be called, otherwise the default status code of 200 will be used
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method Not Allowed"))
+		// or
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create a new snippet..."))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tjandy98/snippetbox
+
+go 1.21.5
