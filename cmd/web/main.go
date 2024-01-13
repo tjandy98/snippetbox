a3df410011b2c3814f80ea540b9aeccc161ea551
tjandy98 <3953059+tjandy98@users.noreply.github.com>
2024-01-13 22:26:46 +0800

    Add db operations

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,12 +8,14 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/tjandy98/snippetbox/internal/models"
 )
 
 // application struct to hold the application-wide dependencies for the web application
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -35,6 +37,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 	srv := &http.Server{
 		Addr:     *addr,
