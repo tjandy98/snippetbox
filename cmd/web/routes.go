5391ec80efc17de03f3800a01a1fe120c4dbec91
tjandy98 <3953059+tjandy98@users.noreply.github.com>
2024-01-14 19:24:23 +0800

    Add middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -11,6 +11,5 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", (*app).snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
-	return mux
+	return app.logRequest(secureHeaders(mux))
 }
